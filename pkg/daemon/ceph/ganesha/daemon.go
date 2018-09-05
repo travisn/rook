eb884db895512d899d7203b17af5b979efb0bd46
travisn <tnielsen@redhat.com>
2018-09-05 14:17:18 -0600

    move ganesha config to operator to be generated
    Signed-off-by: travisn <tnielsen@redhat.com>

--- pkg/daemon/ceph/ganesha/daemon.go
+++ pkg/daemon/ceph/ganesha/daemon.go
@@ -17,11 +17,8 @@ package ganesha
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"strings"
 	"syscall"
 
 	"github.com/coreos/pkg/capnslog"
@@ -32,43 +29,7 @@ import (
 var logger = capnslog.NewPackageLogger("github.com/rook/rook", "ganesha")
 
 const (
-	cephConfigPath    = "/etc/ceph/ceph.conf"
-	ganeshaConfigPath = "/etc/ganesha/ganesha.conf"
-	ganeshaConfig     = `NFS_CORE_PARAM {
-	Enable_NLM = false;
-	Enable_RQUOTA = false;
-	Protocols = 4;
-}
-
-CACHEINODE {
-	Dir_Max = 1;
-	Dir_Chunk = 0;
-	Cache_FDs = false;
-	NParts = 1;
-	Cache_Size = 1;
-}
-
-EXPORT_DEFAULTS {
-	Attr_Expiration_Time = 0;
-}
-
-RADOS_URLS {
-ceph_conf = '$(CEPH_CONFIG_PATH)';
-	userid = '$(USER_ID)';
-}
-# TODO: Replace the rados url with the reference path to /etc/ganesha/ganesha.conf
-#%url rados://mypool/object
-
-NFSv4 {
-RecoveryBackend = 'rados_kv';
-Minor_Versions = 1, 2;
-}
-RADOS_KV {
-ceph_conf = '$(CEPH_CONFIG_PATH)';
-userid = '$(USER_ID)';
-#pool = 'mypool';
-}
-`
+	cephConfigPath = "/etc/ceph/ceph.conf"
 )
 
 type Config struct {
@@ -108,31 +69,7 @@ func generateConfigFiles(context *clusterd.Context, config *Config) error {
 		return fmt.Errorf("failed to write connection config. %+v", err)
 	}
 
-	// save the keyring to the given path
-	if err := os.MkdirAll(filepath.Dir(ganeshaConfigPath), 0744); err != nil {
-		return fmt.Errorf("failed to create ganesha config directory %s: %+v", ganeshaConfigPath, err)
-	}
-	if err := ioutil.WriteFile(ganeshaConfigPath, []byte(generateGaneshaConfig(config)), 0644); err != nil {
-		return fmt.Errorf("failed to ganesha config to %s: %+v", ganeshaConfigPath, err)
-	}
-
 	return nil
-}
-
-func generateGaneshaConfig(config *Config) string {
-	configPath := "/etc/ganesha/config/export.conf"
-	contents, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		logger.Warningf("failed to read file %s. %+v", configPath, err)
-	} else {
-		logger.Infof("config file %s contents:\n%s", configPath, string(contents))
-	}
-
-	// Replace the placeholder settings in the ganesh config
-	r := strings.NewReplacer(
-		"$(USER_ID)", "admin",
-		"$(CEPH_CONFIG_PATH)", cephConfigPath)
-	return r.Replace(ganeshaConfig)
 }
 
 func startGanesha(context *clusterd.Context, config *Config) error {
