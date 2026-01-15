5fa9312c041a05c13ab6119b48af1b0bfe3adfd9
Oded Viner <oviner@redhat.com>
2026-01-15 19:31:55 +0200

    jan15_2
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- pkg/operator/ceph/nvmeof/connectionconfig.sh
+++ pkg/operator/ceph/nvmeof/connectionconfig.sh
@@ -16,12 +16,11 @@ chmod 440 /etc/ceph/keyring
 # ARGS: all args are assumed to be 'ceph' CLI args
 # Use "$@" to pass through Ceph CLI arguments (e.g., --connect-timeout, --mon-host, --keyring, etc.)
 # This allows easy passthru of arguments set by Rook's methods for Ceph CLI options
-GATEWAY_ID="${GATEWAY_NAME:-${POD_NAME}}"
 
 sed -e "s/@@POD_NAME@@/${GATEWAY_ID}/g" \
     -e "s/@@ANA_GROUP@@/${ANA_GROUP}/g" \
     -e "s/@@POD_IP@@/${POD_IP}/g" \
     < /config/nvmeof.conf > /etc/ceph/nvmeof.conf
 
-ceph "$@" nvme-gw create "${GATEWAY_ID}" "${POOL_NAME}" "${ANA_GROUP}" || true
+ceph "$@" nvme-gw create "${GATEWAY_NAME}" "${POOL_NAME}" "${ANA_GROUP}" || true
 ceph "$@" nvme-gw show ${POOL_NAME} ${ANA_GROUP} || true
